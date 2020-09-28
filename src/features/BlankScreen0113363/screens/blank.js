ca16886c263df70963d91ce9fd77460315f4ae87
Crowdbotics <team@crowdbotics.com>
2020-09-28 11:06:10 +0000

    Update components

--- src/features/BlankScreen0113363/screens/blank.js
+++ src/features/BlankScreen0113363/screens/blank.js
@@ -26,22 +26,6 @@ export class _Blank extends React.Component {
 
   render = () => (
     <View style={this.props.themedStyle.View_1}>
-      <View style={this.props.themedStyle.View_3}>
-        <Icon
-          iconFont="Eva Design Icons"
-          name="menu"
-          height={20}
-          width={20}
-          style={this.props.themedStyle.Icon_5}
-        />
-        <Icon
-          iconFont="Eva Design Icons"
-          name="bell-outline"
-          height={20}
-          width={20}
-          style={this.props.themedStyle.Icon_12}
-        />
-      </View>
       <Image
         resizeMode="cover"
         source={{
@@ -84,102 +68,6 @@ export default Blank = withStyles(_Blank, theme => ({
     borderBottomWidth: 0,
     borderRadius: 0,
     backgroundSize: "auto"
-  },
-  View_3: {
-    width: "100%",
-    height: 10,
-    marginLeft: 0,
-    marginRight: 0,
-    marginTop: 10,
-    marginBottom: 10,
-    paddingLeft: 0,
-    paddingRight: 0,
-    paddingTop: 0,
-    paddingBottom: 0,
-    overflow: "visible",
-    fontSize: 12,
-    color: "#000000",
-    backgroundColor: "#ffffff",
-    fontStyle: "normal",
-    fontWeight: "normal",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "space-between",
-    alignItems: "stretch",
-    alignContent: "stretch",
-    borderColor: "#000000",
-    borderStyle: "solid",
-    borderWidth: 0,
-    borderLeftWidth: 0,
-    borderRightWidth: 0,
-    borderTopWidth: 0,
-    borderBottomWidth: 0,
-    borderRadius: 0,
-    backgroundSize: "auto"
-  },
-  Icon_5: {
-    width: 32,
-    height: 32,
-    marginLeft: 0,
-    marginRight: 0,
-    marginTop: 5,
-    marginBottom: 5,
-    paddingLeft: 5,
-    paddingRight: 5,
-    paddingTop: 5,
-    paddingBottom: 5,
-    overflow: "visible",
-    borderColor: "#000000",
-    borderStyle: "solid",
-    borderWidth: 0,
-    borderLeftWidth: 0,
-    borderRightWidth: 0,
-    borderTopWidth: 0,
-    borderBottomWidth: 0,
-    borderRadius: 0,
-    fontSize: 20,
-    color: "#000000",
-    backgroundColor: "#ffffff",
-    fontStyle: "normal",
-    fontWeight: "normal",
-    textAlign: "left",
-    textAlignVertical: "center",
-    textDecorationLine: "none",
-    textTransform: "none",
-    lineHeight: 12,
-    letterSpacing: 0
-  },
-  Icon_12: {
-    width: 32,
-    height: 32,
-    marginLeft: 0,
-    marginRight: 0,
-    marginTop: 5,
-    marginBottom: 5,
-    paddingLeft: 5,
-    paddingRight: 5,
-    paddingTop: 5,
-    paddingBottom: 5,
-    overflow: "visible",
-    borderColor: "#000000",
-    borderStyle: "solid",
-    borderWidth: 0,
-    borderLeftWidth: 0,
-    borderRightWidth: 0,
-    borderTopWidth: 0,
-    borderBottomWidth: 0,
-    borderRadius: 0,
-    fontSize: 20,
-    color: "#000000",
-    backgroundColor: "#ffffff",
-    fontStyle: "normal",
-    fontWeight: "normal",
-    textAlign: "left",
-    textAlignVertical: "center",
-    textDecorationLine: "none",
-    textTransform: "none",
-    lineHeight: 12,
-    letterSpacing: 0
   },
   Image_15: {
     width: 303,
